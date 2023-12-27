496442b50726986ffacdac37794b12c3cf7c0cc8
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-12-27 18:22:33 +0000

    Try using setlocale

--- test_locale.py
+++ test_locale.py
@@ -2,10 +2,6 @@ import locale
 import os
 import sys
 
-##print('setting locale')
-##os.environ['LANGUAGE'] = 'de_DE'
-##locale.setlocale(locale.LC_MESSAGES, '')
-
 import exiv2
 
 def main():
@@ -15,8 +11,9 @@ def main():
     except exiv2.Exiv2Error as ex:
         print(str(ex))
     print('setting locale')
-    os.environ['LANGUAGE'] = 'de_DE'
-    locale.setlocale(locale.LC_MESSAGES, '')
+##    os.environ['LANGUAGE'] = 'de_DE'
+##    locale.setlocale(locale.LC_MESSAGES, '')
+    locale.setlocale(locale.LC_MESSAGES, 'de_DE')
     print('translated')
     try:
         exiv2.ImageFactory.open(__file__)
