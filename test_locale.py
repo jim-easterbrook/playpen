02cb49fb66211f06a00897a44447f0d268c9f347
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-12-27 16:20:48 +0000

    Set locale before import

--- test_locale.py
+++ test_locale.py
@@ -1,12 +1,14 @@
 import locale
+import os
 import sys
+
+print('setting locale')
+locale.setlocale(locale.LC_MESSAGES, 'de_DE')
 
 import exiv2
 
 def main():
     str_en = str_en = 'Failed to read input data'
-    print('setting locale')
-    locale.setlocale(locale.LC_MESSAGES, 'de_DE')
     print('translating')
     exiv2.ImageFactory.open(__file__)
     return 0
